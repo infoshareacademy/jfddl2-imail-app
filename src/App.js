24b8286dfc3e5db5c1dea0ea8af785ab89ece72f
wwwdesigner <jakub.matwiejczyk@gmail.com>
2017-10-05 16:12:42 +0200

    Added router and directory file

--- src/App.js
+++ src/App.js
@@ -1,21 +1,38 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {
+    BrowserRouter as Router,
+    Route,
+} from 'react-router-dom'
+import {
+    Grid
+} from 'react-bootstrap'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+// import MainMenu from './MainMenu' -TUTAJ WPISAĆ KOMPONENTY!
+
+
+class App extends React.Component {
+    render() {
+        return (
+            <Router>
+                <Grid>
+                    <MainMenu/>
+                    <Route path="/tasker" component={Tasker}/>
+                    <Route path="/students" component={Students}/>
+                    <Route path="/groups" component={Groups}/>
+                    <Route
+                        path="/counter"
+                        render={
+                            props =>
+                                <Counter {...props}
+                                         title="Foo"
+                                         delta={10}
+                                         startFrom={0}
+                                />
+                        }/>
+                </Grid>
+            </Router>
+        )
+    }
 }
 
-export default App;
+export default App

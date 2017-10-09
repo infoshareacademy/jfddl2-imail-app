64b3240b382789adcb05b6242cee3476a8f3ac01
lukaszabramczuk <ld.abramczuk@gmail.com>
2017-10-09 16:27:04 +0200

    add base

---
+++ src/Components/ListingBase/ListingBase.js
@@ -0,0 +1,69 @@
+import React from 'react'
+
+
+class ListingBase extends React.Component {
+
+    state = {
+        groups: null,
+        fetching: false,
+        error: null
+    }
+
+    componentDidMount() {
+        this.setState({
+            fetching: true
+        })
+
+        fetch(
+            `${process.env.PUBLIC_URL}/database.json` // template string usage
+        ).then(
+            response => response.json()
+        ).then(
+            groups => this.setState({ groups, fetching: false })
+        ).catch(
+            error => this.setState({ error, fetching: false })
+        )
+    }
+
+    render() {
+        const { groups, error, fetching } = this.state
+
+        return (
+            <div>
+                <h1>Baza</h1>
+                {
+                    groups !== null ?
+                        groups.slice(0,15).map(
+                        ({ id, avatar, fullname, email, gender, city}, index, allGroups) => (
+                            <li key={id}> {++index} {fullname} {email} {gender} {city}<img src={avatar} /></li>
+                        )
+                        ) :
+                        <p>Brak wyników</p>
+                }
+                {
+                    groups !== null ?
+                        null :
+                        <p>Brak wyników</p>
+                }
+
+                {
+                    fetching === false ?
+                        null :
+                        <p>Pobieranie bazy...</p>
+                }
+
+                {
+                    error === null ?
+                        null :
+                        <p>{error.message}</p>
+                }
+            </div>
+        )
+    }
+
+
+}
+
+
+
+export default ListingBase

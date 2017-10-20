f1d701a667f8a5be61f748683a7b8904cff97ebf
DanielJaremek <djaremek11@gmail.com>
2017-10-20 09:09:07 +0200

    fixed SearchForm

--- src/Components/SearchForm/SearchForm.js
+++ src/Components/SearchForm/SearchForm.js
@@ -1,183 +1,187 @@
 import React from 'react'
-import {ToggleButtonGroup, ToggleButton, Table, InputGroup, FormControl,Glyphicon, ButtonToolbar, ButtonGroup, Button} from 'react-bootstrap'
+import {
+  ToggleButtonGroup,
+  ToggleButton,
+  Table,
+  InputGroup,
+  FormControl,
+  Glyphicon,
+  ButtonToolbar,
+  Button
+} from 'react-bootstrap'
 import {Link} from 'react-router-dom'
-
-
+import {connect} from 'react-redux'
+import { database } from '../../firebase'
 const filters = {
-    gender_male: gender => gender.male === 'Mężczyzna',
-    gender_female: gender => gender.female === 'Kobieta'
+  gender_male: gender => gender.male === 'Mężczyzna',
+  gender_female: gender => gender.female === 'Kobieta'
 }
-
 const filterGroups = [
-    {
-        label: 'Mężczyzna',
-        name: 'gender_male'
-    },
-
-    {
-        label: 'Kobieta',
-        name: 'gender_female'
-    }
+  {
+    label: 'Mężczyzna',
+    name: 'gender_male'
+  },
+  {
+    label: 'Kobieta',
+    name: 'gender_female'
+  }
 ]
-
 class SearchForm extends React.Component {
-
-    state = {
-        users: [],
-        fetching: false,
-        error: null,
-        addedUsers: JSON.parse(localStorage.getItem('addedUsers')) || [],
-        searchInput: '',
-        gender: false
-    }
-
-    searchHandler = (event)=>{
-        this.setState({
-            searchInput: event.target.value
-        })
-    }
-
-    genderHandler = (value)=>{
-        console.log(value)
-        this.setState({
-            gender: value
-        })
-    }
-
-    componentDidMount() {
-        this.setState({
-            fetching: true
-        })
-
-        fetch(
-            `${process.env.PUBLIC_URL}/database.json` // template string usage
-        ).then(
-            response => response.json()
-        ).then(
-            users => this.setState({
-                users: users.concat(this.state.addedUsers),
-                fetching: false
-            })
-        ).catch(
-            error => this.setState({error, fetching: false})
-        )
-    }
-
-
-    render() {
-        const {users, error, fetching} = this.props
-
-        const filteredUsers = users.filter((user)=>{
-          return this.state.gender ? user.gender === this.state.gender : true
-        })
-
-        console.log(filteredUsers)
-
-        return (
-            <div>
-                <h1>Wyszukaj</h1>
-                <form>
-                    <InputGroup>
-                        {/*<GroupSearchForm*/}
-                            {/*searchPhrase={this.state.currentSearchPhrase}*/}
-                            {/*handleChange={this.handleSearchPhraseChange }*/}
-                        {/*/>*/}
-                        <FormControl onChange={this.searchHandler} value={this.state.searchInput}/>
-                        <InputGroup.Button>
-                            <Button>
-                                <Glyphicon glyph="search"/> Wyszukaj
-                            </Button>
-                        </InputGroup.Button>
-                    </InputGroup>
-
-                    <ButtonToolbar>
-                        <ToggleButtonGroup onChange={this.genderHandler} type="radio" name="options" defaultValue={false}>
-                            <ToggleButton value={false}>
-                                Wszyscy
-                            </ToggleButton>
-                            <ToggleButton value={'kobieta'}>Kobiety</ToggleButton>
-
-                            <ToggleButton value={'meżczyzna'}>Mężczyźni</ToggleButton>
-                        </ToggleButtonGroup>
-                    </ButtonToolbar>
-                </form>
-
-
-                {
-                    users !== null ?
-                        <Table striped bordered condensed hover style={{
-                            marginTop: 20
-                        }}>
-                            <thead>
-                            <tr>
-                                <th>ID</th>
-                                <th>Imie i nazwisko</th>
-                                <th>Adres e-mail</th>
-                                <th>Miasto</th>
-                                <th>Płeć</th>
-                                {/*<th>Zdjęcie</th>*/}
-                                <th>Szczegóły</th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            {
-                                filteredUsers.filter((user)=>{
-                                    return user.fullname.includes(this.state.searchInput)
-                                    || user.email.includes(this.state.searchInput)
-                                    || user.city.includes(this.state.searchInput)
-                                }).map(
-                                    ({id, fullname, city, gender, email, avatar}, index) => (
-                                        <tr key={id}>
-                                            <td>
-                                              {id}
-                                            </td>
-                                            <td>
-                                                {fullname}
-                                            </td>
-                                            <td>
-                                                {email}
-                                            </td>
-                                            <td>
-                                                {city}
-                                            </td>
-                                            <td>
-                                                {gender}
-                                            </td>
-                                            <td>
-                                                <Link to={'/final-results/' + id}>Zobacz</Link>
-                                            </td>
-                                        </tr>
-                                    )
-                                )
-                            }
-                            </tbody>
-
-                        </Table> :
-                        <p>Brak wyników</p>
-                }
-
-                {
-                    users !== null ?
-                        null :
-                        <p>Brak wyników</p>
-                }
-
-                {
-                    fetching === false ?
-                        null :
-                        <p>Pobieranie bazy...</p>
-                }
-
-                {
-                    error === null ?
-                        null :
-                        <p>{error.message}</p>
-                }
-            </div>
-        )
-    }
-
-
+  state = {
+    users: [],
+    fetching: false,
+    error: null,
+    addedUsers: JSON.parse(localStorage.getItem('addedUsers')) || [],
+    searchInput: '',
+    gender: false
+  }
+  searchHandler = (event) => {
+    this.setState({
+      searchInput: event.target.value
+    })
+  }
+  genderHandler = (value) => {
+    console.log(value)
+    this.setState({
+      gender: value
+    })
+  }
+  componentDidMount() {
+    this.setState({
+      fetching: true
+    })
+    database().ref('contacts').on('value', snapshot => {
+          let users = snapshot.val()
+          this.setState({
+            users: users.concat(this.state.addedUsers),
+            fetching: false
+          })
+        }
+    )
+  }
+  mapStateToProps = props => (
+      <div>
+          <ul>
+            {
+              props.messages && Object.entries(props.messages).reverse().map(
+                  ([key, value]) => (
+                      <li key={key}>
+                          <p>{value.content}</p>
+                          <p><strong>{value.author}</strong></p>
+                      </li>
+                  )
+              )
+            }
+          </ul>
+      </div>
+  )
+  render() {
+    const filteredUsers = this.state.users.filter((user) => {
+      return this.state.gender ? user.gender === this.state.gender : true
+    })
+    console.log(filteredUsers)
+    return (
+        <div>
+            <h1>Wyszukaj</h1>
+            <form>
+                <InputGroup>
+                  {/*<GroupSearchForm*/}
+                  {/*searchPhrase={this.state.currentSearchPhrase}*/}
+                  {/*handleChange={this.handleSearchPhraseChange }*/}
+                  {/*/>*/}
+                    <FormControl onChange={this.searchHandler} value={this.state.searchInput}/>
+                    <InputGroup.Button>
+                        <Button>
+                            <Glyphicon glyph="search"/> Wyszukaj
+                        </Button>
+                    </InputGroup.Button>
+                </InputGroup>
+                <ButtonToolbar>
+                    <ToggleButtonGroup onChange={this.genderHandler} type="radio" name="options"
+                                       defaultValue={false}>
+                        <ToggleButton value={false}>
+                            Wszyscy
+                        </ToggleButton>
+                        <ToggleButton value={'kobieta'}>Kobiety</ToggleButton>
+                        <ToggleButton value={'meżczyzna'}>Mężczyźni</ToggleButton>
+                    </ToggleButtonGroup>
+                </ButtonToolbar>
+            </form>
+          {
+            this.state.users !== null ?
+                <Table striped bordered condensed hover style={{
+                  marginTop: 20
+                }}>
+                    <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Imie i nazwisko</th>
+                        <th>Adres e-mail</th>
+                        <th>Miasto</th>
+                        <th>Płeć</th>
+                      {/*<th>Zdjęcie</th>*/}
+                        <th>Szczegóły</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {
+                      filteredUsers.filter((user) => {
+                        return user.fullname.includes(this.state.searchInput)
+                            || user.email.includes(this.state.searchInput)
+                            || user.city.includes(this.state.searchInput)
+                      }).map(
+                          ({id, fullname, city, gender, email, avatar}, index) => (
+                              <tr key={id}>
+                                  <td>
+                                    {id}
+                                  </td>
+                                  <td>
+                                    {fullname}
+                                  </td>
+                                  <td>
+                                    {email}
+                                  </td>
+                                  <td>
+                                    {city}
+                                  </td>
+                                  <td>
+                                    {gender}
+                                  </td>
+                                  <td>
+                                      <Link to={'/final-results/' + id}>Zobacz</Link>
+                                  </td>
+                              </tr>
+                          )
+                      )
+                    }
+                    </tbody>
+                </Table> :
+                <p>Brak wyników</p>
+          }
+          {
+            this.state.users !== null ?
+                null :
+                <p>Brak wyników</p>
+          }
+          {
+            this.state.fetching === false ?
+                null :
+                <p>Pobieranie bazy...</p>
+          }
+          {
+            this.state.error === null ?
+                null :
+                <p>{this.state.error.message}</p>
+          }
+        </div>
+    )
+  }
 }
-
+// const mapStateToProps = state => ({
+//     users: state.users
+// })
+//
+// export default connect(
+//     mapStateToProps
+// )(SearchForm)
 export default SearchForm

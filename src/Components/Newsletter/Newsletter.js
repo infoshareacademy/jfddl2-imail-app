0c7ec0ab6d4793504c4af6b7a3c0dac4a8cd83cb
wwwdesigner <jakub.matwiejczyk@gmail.com>
2017-10-26 16:30:29 +0200

    email API works

--- src/Components/Newsletter/Newsletter.js
+++ src/Components/Newsletter/Newsletter.js
@@ -7,43 +7,31 @@ class Newsletter extends React.Component {
     }
 
     handleEmailButton = (event) => {
-        const init = {
-            method: 'POST',
-            mode: 'no-cors',
+        fetch("https://tranquil-thicket-80023.herokuapp.com", {
+            method: "POST",
+            mode: 'cors',
             headers: {
-                "Content-Type": "application/json",
-                "authorization": "Basic YTZlZDk1YTZiZGMzMDRkOGExYzYxZTc4NWE1YzE0MmY5MzU0YmZjNDozNzBhOGMyMzVmNzdkYmUxMmYwNjFmMjAwN2NjNjlhZmM2MDI2ZTM3"
+                'Content-Type': 'application/json'
             },
             body: JSON.stringify({
-                "to": this.props.email,
-                "subject": "Powitanie",
-                "text": "Witamy serdecznie w Instant Mail.",
-                "from": "Instant Mail"
+                "emails": [this.props.email],
+                "subject": "test",
+                "message": "Pozdrowienia od Andrzeja!",
+                "from": "instantmail@imail.com",
+                "fromName": "iMailApp"
             })
-        };
-
-        console.log(init)
-
-        fetch('https://api.emaillabs.net.pl/api/new_sendmail', init).then(
-            response => {
-                if (response.ok) {
-                    return response.json();
-                }
-                throw Error('Api response not ok', response
-                )
-            }
-        ).catch(
-            error => console.error(error)
-        );
+        }).then(response => response.json())
+            .then(console.log)
+    }
+        render()
+        {
+            return (
+                <button onClick={this.handleEmailButton}>
+                    Wyślij e-mail
+                </button>
+            )
+        }
     }
 
-    render() {
-        return (
-            <button onClick={this.handleEmailButton}>
-                Wyślij e-mail
-            </button>
-        )
-    }
-}
 
 export default Newsletter
